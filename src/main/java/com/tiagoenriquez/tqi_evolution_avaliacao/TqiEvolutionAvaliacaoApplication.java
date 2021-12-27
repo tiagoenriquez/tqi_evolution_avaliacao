6e15de52f66ee4777e044cccd1bf373957fd988b
tiagoenriquez <tiagoenriquez@gmail.com>
2021-12-26 22:43:05 -0300

    criando o repositório do projeto

---
+++ src/main/java/com/tiagoenriquez/tqi_evolution_avaliacao/TqiEvolutionAvaliacaoApplication.java
@@ -0,0 +1,13 @@
+package com.tiagoenriquez.tqi_evolution_avaliacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TqiEvolutionAvaliacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TqiEvolutionAvaliacaoApplication.class, args);
+	}
+
+}
