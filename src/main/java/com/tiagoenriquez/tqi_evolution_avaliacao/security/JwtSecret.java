f165f569384923e9ae0a28501e312691df1cc0d2
tiagoenriquez <tiagoenriquez@gmail.com>
2022-01-08 16:13:53 -0300

    adicionando arquivos de configuração

--- src/main/java/com/tiagoenriquez/tqi_evolution_avaliacao/security/JwtSecret.java
+++ src/main/java/com/tiagoenriquez/tqi_evolution_avaliacao/security/JwtSecret.java
@@ -1,20 +1,14 @@
 package com.tiagoenriquez.tqi_evolution_avaliacao.security;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.stereotype.Component;
-
-//@Component
-@ConfigurationProperties("jwt.secret")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class JwtSecret {
 
-    //@Value("${jwt.secret}")
-    private String secret;
+    /**
+     * Valor obtido da variável de ambiente.
+     */
+    private static String SECRET = "${JWT_SECRET}";
+
+    public String getSecret() {
+        return this.SECRET;
+    }
 
 }
